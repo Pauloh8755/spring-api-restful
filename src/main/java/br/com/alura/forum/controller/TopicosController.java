242e805cd1c6639254bdfebbfb27105b7d7e4e46
Paulo Andrade <paulo.andrade@accurate.com.br>
2022-03-08 11:29:25 -0300

    feat: Boas praticas no cadastro

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
@@ -47,10 +50,18 @@ public class TopicosController {
     //definindo verbo http
     @PostMapping
     //@requestBody para indicar que os parametros serão recebidos pelo body do request
-    public void cadastrar(@RequestBody TopicoForm form){
+    //ResponseEntity<> para definir um response personalizado
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder){
         //converter form em topico
         Topico topico = form.converter(cursoRepository);
         //gravando topico no banco de dados
         topicoRepository.save(topico);
+        /*Response 201: Ao enviar um codigo 201 ao cliente, devemos devolver 2 elementos.
+         *  1° cabeçalho http chamado location com a url do recurso criado
+         *  2° Uma representação desse recurso no body do response 
+         */
+        //Criando uri por meio do uriBuilder
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri)
     }
 }
